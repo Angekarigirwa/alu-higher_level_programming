3dd0b004a4f2d4150c670138a0b5e55f1ce3928e
Angekarigirwa <a.karigirwa@alustudent.com>
2025-03-03 05:41:52 -0800

    updated

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    div = 1
-    try:
-        div = a / b
-
-    except:
-        div = None
-    finally:
-        print("Inside result: {}".format(div))
-        return div
+def list_division(my_list_1, my_list_2, list_length):
+    my_list_3 = list()
+    for k in range(list_length):
+        try:
+            val = my_list_1[k] / my_list_2[k]
+        except ZeroDivisionError:
+            val = 0
+            print("division by 0")
+        except TypeError:
+            val = 0
+            print("wrong type")
+        except IndexError:
+            val = 0
+            print("out of range")
+        finally:
+            my_list_3.append(val)
+            continue
+    return my_list_3
