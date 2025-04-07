efb40253ee78167dc5594bef76c7fd6cfcf115d9
Angekarigirwa <a.karigirwa@alustudent.com>
2025-04-07 04:32:03 -0700

    Done

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""
-Class definition of a City
-"""
+"""class State and instance base"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(Base):
-    """
-    City class that inherits from Base
-
-    Attributes:
-        id: Id city
-        name: Name of the city
-        state_id: State id
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+class State(Base):
+    """instance Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
